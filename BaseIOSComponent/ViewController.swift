403f977799f0daaceb67829c78a7feab8741c06c
liyang31tg <liyang@macBookProdeMacBook-Pro.local>
2015-12-30 21:25:40 +0800

    weChat image
     导入微信app的图片

--- BaseIOSComponent/ViewController.swift
+++ BaseIOSComponent/ViewController.swift
@@ -12,16 +12,7 @@ import Foundation
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let client = WSClient()//wep.webuy-china.com/websocket
-        client.startConnect(NSURL(string: "ws://wep.webuy-china.com/websocket?accountId=100238&password=54A09E52FE7E064F&deviceId=7A11056B-522C-47DE-A12B-C81263226C05&pduVersion=44&deviceToken=&iOSVppClient=0&appVersion=3973&pduParserVersion=2&deviceType=ios/")!)
-        
-//        let socket = SocketIOClient(socketURL: "localhost:8880")
-//        socket.on("important message") {data, ack in
-//            println("Message for you! \(data?[0])")
-//            ack?("I got your message, and I'll send my response")
-//            socket.emit("response", "Hello!")
-//        }
-//        socket.connect()
+        print("af");
         
     
     }
---
+++ BaseIOSComponent/public/BaseViewController/BaseNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.swift
+//  BaseIOSComponent
+//
+//  Created by liyang on 15/12/30.
+//  Copyright © 2015年 liyang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class BaseNavigationController: UINavigationController {
+    
+}
